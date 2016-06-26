fdfb97a3e3bc9653428746cf5604fa84e5411c72
lishulei <lishulei@huoyunren.com>
2016-06-26 15:45:36 +0800

    modify config

--- config/my.php
+++ config/my.php
@@ -2,7 +2,7 @@
 
 return [
     'paginate' => 5,
-    'title' => '舒磊空间',
+    'title' => '学习笔记',
     'description' => '',
     'posts_path' => public_path() . '/source/posts', 
     'about_path' => public_path() . '/source/aboutme/index.md',
